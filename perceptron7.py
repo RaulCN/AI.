3882c587aadf61bc3574e80957bc56b223a71721
Raul Campos Nascimento <31525836+RaulCN@users.noreply.github.com>
2017-09-25 14:20:28 -0300

    Rename perceptron6.py to perceptron7.py

---
+++ perceptron7.py
@@ -0,0 +1,90 @@
+#Código atualizado para python 3
+#Fonte http://www.cs.colostate.edu/~cs545/fall15/doku.php?id=code:perceptron
+#Falta traduzir os comentários para maior compreensão da galera do Brasil
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Perceptron :
+
+    """An implementation of the perceptron algorithm.
+    Note that this implementation does not include a bias term"""
+
+    def __init__(self, max_iterations=100, learning_rate=0.2) :
+
+        self.max_iterations = max_iterations
+        self.learning_rate = learning_rate
+
+    def fit(self, X, y) :
+        """
+        Train a classifier using the perceptron training algorithm.
+        After training the attribute 'w' will contain the perceptron weight vector.
+
+        Parameters
+        ----------
+
+        X : ndarray, shape (num_examples, n_features)
+        Training data.
+
+        y : ndarray, shape (n_examples,)
+        Array of labels.
+        
+        """
+        self.w = np.zeros(len(X[0]))
+        converged = False
+        iterations = 0
+        while (not converged and iterations < self.max_iterations) :
+            converged = True
+            for i in range(len(X)) :
+                if y[i] * self.discriminant(X[i]) <= 0 :
+                    self.w = self.w + y[i] * self.learning_rate * X[i]
+                    converged = False
+                    plot_data(X, y, self.w)
+            iterations += 1
+        self.converged = converged
+        if converged :
+            print ('convergiu em %d interações' % iterations)
+
+    def discriminant(self, x) :
+        return np.dot(self.w, x)
+            
+    def predict(self, X) :
+        """
+        make predictions using a trained linear classifier
+
+        Parameters
+        ----------
+
+        X : ndarray, shape (num_examples, n_features)
+        Training data.
+        """
+        
+        scores = np.dot(self.w, X)
+        return np.sign(scores)
+
+def generate_separable_data(N) :
+    xA,yA,xB,yB = [np.random.uniform(-1, 1) for i in range(4)]
+    w = np.random.uniform(-1, 1, 2)
+    print (w,w.shape)
+    X = np.random.uniform(-1, 1, [N, 2])
+    print (X,X.shape)
+    y = np.sign(np.dot(X, w))
+    return X,y,w
+    
+def plot_data(X, y, w) :
+    fig = plt.figure(figsize=(5,5))
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+    a = -w[0]/w[1]
+    pts = np.linspace(-1,1)
+    plt.plot(pts, a*pts, 'k-')
+    cols = {1: 'r', -1: 'b'}
+    for i in range(len(X)): 
+        plt.plot(X[i][0], X[i][1], cols[y[i]]+'o')
+    plt.show()
+
+if __name__=='__main__' :
+    X,y,w = generate_separable_data(40)
+    p = Perceptron()
+    p.fit(X,y)
