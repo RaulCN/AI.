02e1781c21f696d2f3351edb1c26c2f7019e8cad
Topiarau <31525836+Topiarau@users.noreply.github.com>
2018-11-28 13:41:13 -0200

    Update perceptronespanholNF.py
    Traduzindo

--- perceptronespanholNF.py
+++ perceptronespanholNF.py
@@ -1,34 +1,35 @@
+#pra que ele serve?
 import numpy as np
 
 class Perceptron:
     """Clasificador Perceptron baseado na descrição do livro
-    "Python Machine Learning" de Sebastian Raschka.
+    "Python Machine Learning" de Sebastian Raschka. (qual página?)
     Parametros
     fonte desse código:
     https://gist.github.com/Edux87/a6793ce1041a62ae8a08c761a9478cd5
     ----------
     eta: float
-        Tasa de aprendizaje.
+        Taxa de aprendizagem.
     n_iter: int
         Pasadas sobre el dataset.
     Atributos
     ---------
     w_: array-1d
-        Pesos actualizados después del ajuste
+        Pesos actualizados depois do ajuste
     errors_: list
-        Cantidad de errores de clasificación en cada pasada
+        Quantidade de erros de classificação em cada pasada
     """
     def __init__(self, eta=0.1, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        """Ajustar dadoos de treinamento
-        Parámetros
+        """Ajustar dados de treinamento
+        Parâmetros
         ----------
         X:  array like, forma = [n_samples, n_features]
-            Vectores de entrenamiento donde n_samples es el número de muestras y
-            n_features es el número de carácteristicas de cada muestra.
+            Vetores de entrenamiento onde n_samples é o número de muestras e
+            n_features é o número de carácteristicas de cada muestra.
         y:  array-like, forma = [n_samples].
             Valores de destino
         n_samples = muestras (filas)
@@ -51,14 +52,14 @@ class Perceptron:
         return self
 
     def predict(self, X):
-        """Devolver clase usando función escalón de Heaviside.
-        phi(z) = 1 si z >= theta; -1 en otro caso
+        """Devolver clase usando função escalón de Heaviside.
+        phi(z) = 1 si z >= theta; -1 em outro caso
         """
         phi = np.where(self.net_input(X) >= 0.0, 1, -1)
         return phi
 
     def net_input(self, X):
-        """Calcular el valor z (net input)"""
+        """Calcular o valor z (net input)"""
         # z = w · x + theta
         z = np.dot(X, self.w_[1:]) + self.w_[0]
         return z
@@ -67,11 +68,11 @@ class Perceptron:
         """Test classifier on samples, and returns error/total percentage."""
         errors = sum(1 for Xi, label in zip(X, y)
                     if self.predict(Xi) != label)
-        return 100 * (errors / len(X
+        return 100 * (errors / len(X))
         
-// TEST
+# TEST
 
-ppn = Perceptron(eta=1, n_iter=2) #n está funcionando aki
+ppn = Perceptron(eta=1, n_iter=2) 
 
 
 X = [
@@ -104,4 +105,4 @@ ppn.fit(np.array(X), y)
 print(ppn.predict([1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1]))
 print(ppn.test(np.array(X), y))
 
-// rodar em  python3
+# rodar em  python3
