eb0e91a0d7a2a3d84f41e236792a70884b19b7a5
Topiarau <31525836+Topiarau@users.noreply.github.com>
2018-12-04 11:57:50 -0200

    add comentários

--- plotF.py
+++ plotF.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
+#passos simples para criar um gráfico no matplotlib
+import matplotlib.pyplot as plt #importando a biblioteca gráfica
+import numpy as np #importando a biblioteca para operações matemáticas
 
-x = np.linspace(0, 3, 20)
-y = np.linspace(0, 9, 20)
-plt.plot(x, y)       # plotagem da linha
-plt.plot(x, y, 'o')  # plotagem dos pontos
+x = np.linspace(0, 3, 20) #add os pontos nas das coordenadas
+y = np.linspace(0, 9, 20) #add os pontos nas absissas
+plt.plot(x, y, 'b')       # plotagem da linha
+plt.plot(x, y, 'o')  # plotagem dos pontos #  https://goo.gl/UqyxhC
 plt.show()           # <-- mostra o gráfico
